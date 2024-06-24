a3c50abc56f854dbfd71f1327a7dbbc1f5b31426
muhammadfikri4 <unicornhorse1@gmail.com>
2024-06-25 00:04:50 +0700

    revoke react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
 import "mapbox-gl/dist/mapbox-gl.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={createBrowserRouter([{ path: "/", element: <App /> }])} />
+    {/* <RouterProvider router={createBrowserRouter([{ path: "/", element: <App /> }])} /> */}
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+    {/* <App /> */}
   </React.StrictMode>
 );
