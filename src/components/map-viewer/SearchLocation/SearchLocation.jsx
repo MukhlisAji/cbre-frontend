556e0360c01242eccb0ce74f8ef59a906e64ba90
rajifmahendra10 <rajifmahendra10@gmail.com>
2024-07-29 15:07:08 +0700

    update

--- src/components/map-viewer/SearchLocation/SearchLocation.jsx
+++ src/components/map-viewer/SearchLocation/SearchLocation.jsx
@@ -157,9 +157,9 @@ const SearchLocation = ({
           <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
         </svg>
       </form>
-      <div className="z-[9999999]">
+      {/* <div className="z-[9999999]">
         <button>TES</button>
-      </div>
+      </div> */}
       {isFocused && (history.length > 0 || search.length > 0) && (
         <div className="search-results">{renderList()}</div>
       )}
