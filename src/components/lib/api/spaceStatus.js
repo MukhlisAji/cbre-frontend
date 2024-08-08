c231a0d7808416e2f0714ba4017581c1c9af1bb9
rajifmahendra <rajifmahendra10@gmail.com>
2024-08-08 09:06:02 +0700

    update 3d

--- src/components/lib/api/spaceStatus.js
+++ src/components/lib/api/spaceStatus.js
@@ -4,6 +4,7 @@ import { CONFIG_APP } from "../../map-viewer/config/app";
 export function spaceStatus() {
   const [data, setData] = useState(null);
   useEffect(() => {
+    //api master
     fetch(`${CONFIG_APP.MAPBOX_API}/test`)
       .then((res) => res.json())
       .then((res) => setData(res));
