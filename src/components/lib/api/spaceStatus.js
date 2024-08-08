bd6ccd2591c21a27f21c57ef76996f13a7550a48
rajifmahendra <rajifmahendra10@gmail.com>
2024-08-08 10:52:36 +0700

    update config

--- src/components/lib/api/spaceStatus.js
+++ src/components/lib/api/spaceStatus.js
@@ -1,13 +1,21 @@
-import { useEffect, useState } from "react";
-import { CONFIG_APP } from "../../map-viewer/config/app";
+import {
+  useEffect,
+  useState
+} from "react";
+import {
+  CONFIG_APP
+} from "../../map-viewer/config/app";
 
 export function spaceStatus() {
   const [data, setData] = useState(null);
   useEffect(() => {
-    //api master
-    fetch(`${CONFIG_APP.MAPBOX_API}/test`)
-      .then((res) => res.json())
-      .then((res) => setData(res));
+      //api master
+      fetch(`${CONFIG_APP.MAPBOX_API}/test`)
+          .then((res) => res.json())
+          .then((res) => setData(res));
   });
-  return { data, setData };
+  return {
+      data,
+      setData
+  };
 }
