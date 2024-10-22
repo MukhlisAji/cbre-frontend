60a02d6a11bf702acd81c5f4d750b46ae84bdc6f
rajifmahendra258 <rajif.mahendra@cbre.com>
2024-10-22 16:45:52 +0700

    update powerbi frame

--- src/components/analytics/pages/PowerBI.jsx
+++ src/components/analytics/pages/PowerBI.jsx
@@ -6,9 +6,7 @@ import { HTML5Backend } from 'react-dnd-html5-backend';
 export default function PowerBI() {
   return (
     <DndProvider backend={HTML5Backend}>
-      <div className="flex w-full h-full overflow-hidden justify-stretch items-stretch">
-      <iframe title="Salesy" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=88b98544-4187-4c85-99d4-707c9d7084de&autoAuth=true&ctid=f6153fa6-50c1-44e9-9691-1fdcc84a36aa" frameborder="0" allowFullScreen="true"></iframe>
-      </div>
+      <iframe title="test" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiNTk3ZTZjZjctYmU1YS00MDQwLThjMjgtNTBlZjU2ZDZlNDhlIiwidCI6IjM0ODViOTYzLTgyYmEtNGE2Zi04MTBmLWI1Y2MyMjZmZjg5OCIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
     </DndProvider>
   );
 }
